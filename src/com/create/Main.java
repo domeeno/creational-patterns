9452bc4561de3c55847d5f5f5650135c57501e9b
dominatorfaf <dominicflocea.dev@gmail.com>
2020-11-11 22:17:41 +0200

    Added thread safety to the Singleton

--- src/com/create/Main.java
+++ src/com/create/Main.java
@@ -6,9 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         MySQLConnection instance = MySQLConnection.getInstance();
-        MySQLConnection instance2 = MySQLConnection.getInstance();
 
-        System.out.println(instance);
-        System.out.println(instance2);
     }
 }
--- src/com/create/connection/MySQLConnection.java
+++ src/com/create/connection/MySQLConnection.java
@@ -1,13 +1,21 @@
 package com.create.connection;
 
 public class MySQLConnection {
-    private static MySQLConnection instance = null;
+    private static volatile MySQLConnection instance = null;
 
-    private MySQLConnection () {}
+    private MySQLConnection () {
+        if(instance != null) {
+            throw new RuntimeException("Instance of DB is already running");
+        }
+    }
 
     public static MySQLConnection getInstance() {
         if(instance == null) {
-            instance = new MySQLConnection();
+            synchronized (MySQLConnection.class) {
+                if(instance == null) {
+                    instance = new MySQLConnection();
+                }
+            }
         }
 
         return instance;
